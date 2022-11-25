3d7d66f9bf2e244b5c15dcac90ab073f6b601bce
autelkhan <utelkhan.aza@gmail.com>
2022-11-25 17:10:14 +0600

    clients-list.component.ts --> определил тип для переменной clientData. Убрал лишние коментарии. clients-list.component.html --> убрал лишние коменатии.

--- src/app/components/clients-list/clients-list.component.ts
+++ src/app/components/clients-list/clients-list.component.ts
@@ -21,7 +21,7 @@ export class ClientsListComponent implements OnInit, AfterViewInit, OnDestroy {
   displayedColumns: string[] = ['id', 'name', 'charm', 'birth_date', 'total_account_balance', 'maximum_balance', 'minimum_balance', 'Options'];
   dataSource!: MatTableDataSource<Client>;
   // todo: Тут должен быть определенная типизация. Нельзя ставить any
-  clientData!: any;
+  clientData!: Client[];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   sub!: Subscription;
@@ -102,63 +102,3 @@ export class ClientsListComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
 }
-
-// todo: Лишние комментарии убрать
-// export class ClientsListComponent implements OnInit, AfterViewInit {
-//   displayedColumns: string[] = ['fullName', 'charm', 'age', 'grandTotalOfBalance', 'maximumBalance', 'minimumBalance', 'options'];
-//   clients!: MatTableDataSource<Client>;
-//   today: Date;
-//   @ViewChild(MatSort) sort: MatSort;
-//
-//   constructor(private liveAnnouncer: LiveAnnouncer, private dialog: MatDialog, private clientService: ClientService) {}
-//
-//   ngOnInit(): void {
-//     this.today = new Date();
-//     this.getClients();
-//   }
-//   ngAfterViewInit() {
-//     const dataSourceClients = new MatTableDataSource(this.clients);
-//     dataSourceClients.sort = this.sort;
-//     this.clients = dataSourceClients.data;
-//   }
-//   announceSortChange(sortState: Sort) {
-//     if (sortState.direction) {
-//       this.liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-//     } else {
-//       this.liveAnnouncer.announce('Sorting cleared');
-//     }
-//   }
-//
-//   getClients() {
-//     this.clientService.getClients().subscribe((clientsList) => {
-//       this.clients = new MatTableDataSource(clientsList);
-//     });
-//   }
-//
-//   openAddClientDialog(): void {
-//     const dialogRef = this.dialog.open(AddClientComponent, {
-//       width: '50%'
-//     });
-//     dialogRef.afterClosed().subscribe((type) => {
-//       if (type === 'add') {
-//         this.getClients();
-//       }
-//     });
-//   }
-//
-//   openEditClientDialog(client: Client): void {
-//     const dialogRef = this.dialog.open(AddClientComponent, {
-//       width: '50%',
-//       data: client
-//     });
-//     dialogRef.afterClosed().subscribe((type) => {
-//       if (type === 'update') {
-//         this.getClients();
-//       }
-//     });
-//   }
-//   deleteClient(id: number) {
-//     this.clientService.deleteClient(id);
-//     this.getClients();
-//   }
-// }
